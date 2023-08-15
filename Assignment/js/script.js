8da69c3392cbd0395fb155c9bab0afddd3f8b429
rob90zhuang <142299132+rob90zhuang@users.noreply.github.com>
2023-08-15 01:29:50 -0700

    Add files via upload

---
+++ Assignment/js/script.js
@@ -0,0 +1,10 @@
+
+    var alas = document.getElementById('as');
+    var tinggi = document.getElementById('at');
+    var hasil = document.getElementById('HasilLuas');
+
+function LuasSegitiga()
+{
+    hasil = alas * tinggi / 2;
+    HasilLuas.innerHTML = hasil;
+}
